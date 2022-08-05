4c68c8b3dbcfe21e3d1ed5af90b768b92c1baeab
PASQUET Fabien <fab.pasquet@gmail.com>
2022-08-05 19:40:25 +0200

    ✨  add divider component

---
+++ packages/ui/src/components/Atoms/Divider/Divider.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import React from 'react';
+
+import { spacingSystemPropsControls } from '../../../constants';
+import { Divider } from './Divider';
+
+export default {
+  title: 'Components/Atoms/Divider',
+  component: Divider,
+  argTypes: {
+    ...spacingSystemPropsControls
+  }
+} as ComponentMeta<typeof Divider>;
+
+const Template: ComponentStory<typeof Divider> = (args) => (
+  <Divider {...args} />
+);
+
+export const Overview = Template.bind({});
+Overview.args = {};
