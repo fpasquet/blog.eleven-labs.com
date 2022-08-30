76efdde416facd9767d5096b4753394584d5bad1
PASQUET Fabien <fab.pasquet@gmail.com>
2022-08-30 08:24:03 +0200

    👷  integration of lighthouse ci and performance-budgets

---
+++ .github/workflows/lighthouse-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: 'lighthouse:default',
+  settings: {
+    maxWaitForFcp: 15 * 1000,
+    maxWaitForLoad: 35 * 1000,
+    skipAudits: ['uses-http2'],
+  },
+  audits: [
+    'metrics/first-contentful-paint-3g',
+  ],
+  categories: {
+    performance: ({
+      auditRefs: [
+        { id: 'first-contentful-paint-3g', weight: 0 },
+      ],
+    }),
+  },
+};
